62d8eda3c4519d4c2e028e6871c164cfb7e41501
Thad <Thadphillp35@gmail.com>
2026-05-14 12:17:19 -0400

    done

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
 class MathFunction { 
-    constructor(name) {
-        this.MathFunction = name;
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    add() {
+        return this.a + this.b;
+    }
+
+    subtract() {
+        return this.a + this.b;
+    }
+
+    multiply() {
+        return this.a + this.b;
+    }
+
+    divide() {
+        return this.a + this.b;
     }
 }
-add() {
-return ""
-}
-subtract() {
 
+class Greetings {
+    constructor(name) {
+        this.name = name;
+    }
+    
+    english() {
+        return "Hello " + this.name;
+    }
+    
+    spanish() {
+        return "Hola " + this.name;
+    }
+    
+    french() {
+        return "Bonjour " + this.name;
+    }
+    
+    arabic() {
+        return "مرحبا " + this.name;
+    }
+}
+
+const mathCalc = new MathFunction(5, 3);
+console.log(mathCalc.add());
+
+const greet = new Greetings("Josh");
+console.log(greet.english());
